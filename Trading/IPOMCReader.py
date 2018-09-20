e1b81e8d3ac1502ef708dc02e872faf9df26c946
Srikanthu <sribala.2001@gmail.com>
2018-09-20 10:59:21 +0530

    added ipo details

--- Trading/IPOMCReader.py
+++ Trading/IPOMCReader.py
@@ -20,8 +20,8 @@ def get_listed_ipo(stock_url):
     # Variables declaration
     start = time.time()
     # Get the shares from chitt
-    # urls = get_list_of_urls(stock_url)
-    urls = ['http://www.chittorgarh.com/ipo/ipo_perf_tracker.asp', 'http://www.chittorgarh.com/ipo/ipo_perf_tracker.asp?FormIPOPT_Page=2', 'http://www.chittorgarh.com/ipo/ipo_perf_tracker.asp?FormIPOPT_Page=3', 'http://www.chittorgarh.com/ipo/ipo_perf_tracker.asp?FormIPOPT_Page=4']
+    urls = get_list_of_urls(stock_url)
+    # urls = ['http://www.chittorgarh.com/ipo/ipo_perf_tracker.asp', 'http://www.chittorgarh.com/ipo/ipo_perf_tracker.asp?FormIPOPT_Page=2', 'http://www.chittorgarh.com/ipo/ipo_perf_tracker.asp?FormIPOPT_Page=3', 'http://www.chittorgarh.com/ipo/ipo_perf_tracker.asp?FormIPOPT_Page=4']
     print(urls)
     cpdus = multi.cpu_count()
     page_bins = h.chunks(cpdus, urls)
@@ -38,8 +38,14 @@ def get_listed_ipo(stock_url):
     print("All jobs completed......")
     try:
         result_list = [x.recv() for x in spipe_list]
+        final_list = []
         for results in result_list:
-            print("results = ", results)
+            for data in results:
+                values = [data[k] for k in data]
+                final_list.extend(values)
+
+        print("IPOs listed so far = {}".format(len(final_list)))
+        print(final_list)
     except Exception as err:
         traceback.print_exc()
         print(str(err))
@@ -67,6 +73,28 @@ def process_page(data_array, send_end):
     send_end.send(results)
 
 
+def get_nse_code(url):
+    nse_code, isin_code = None, None
+    try:
+        bs = h.parse_url(url)
+        if bs:
+            divs = bs.findAll("div", {'class': "panel panel-default"})[2].find('div', {'class': 'panel-body'})
+            sub_div = divs.descendants
+            for div in sub_div:
+                if div.name == "li":
+                    value = div.text
+                    if "NSE Symbol:" in value:
+                        nse_code = value.split(":")[1].strip()
+                    elif "ISIN:" in value:
+                        isin_code = value.split(":")[1].strip()
+            if not nse_code:
+                nse_code = isin_code
+
+    except Exception as err:
+        print("While parsing for NSE code", str(err))
+    return nse_code, isin_code
+
+
 def get_ipo_day_info(link):
     ipo_details = []
     try:
@@ -85,13 +113,13 @@ def get_ipo_day_info(link):
 
             table = bs.find("table", {"class": "table table-condensed table-bordered table-striped table-nonfluid"})
             rows = table.findAll("tr")
-            curr_price = rows[1].findAll("td")[0].find('span').text.strip()
+            # curr_price = rows[1].findAll("td")[0].find('span').text.strip()
             open_price = rows[2].findAll("td")[1].text.strip()
             hl = rows[3].findAll("td")[1].text.split("-")
             high, low = hl[0].strip(), hl[1].strip()
             prev_price = rows[4].findAll("td")[1].text.strip()
             turn_over = rows[8].findAll("td")[1].text.strip()
-            ipo_details.append(curr_price)
+            # ipo_details.append(curr_price)
             ipo_details.append(open_price)
             ipo_details.append(high)
             ipo_details.append(low)
@@ -113,13 +141,19 @@ def get_listed_details(element):
         if ": " in record:
             if not "Change(Rs):" in record:
                 day_trade.append(record.split(":")[1].strip())
-    print("day_trade = ", day_trade)
     url = None
     for a in element.find_all('a', href=True):
         if "http://www.chittorgarh.com/ipo/" in a['href']:
             url = a['href']
+    print("FINAL URL = ", url)
+    if url:
+        nse_code, isin_code = get_nse_code(url)
+        day_trade.insert(0, nse_code)
+        day_trade.insert(1, isin_code)
+    print("TRADE = ", day_trade)
+    return day_trade
 
-    return day_trade, url
+
 def get_ipo_details_perf(url):
 
     ipo_details = {}
@@ -141,20 +175,31 @@ def get_ipo_details_perf(url):
                         i += 1
                         if i == 1: __key = ele.text.strip()
                         __val = ele.text.strip()
+                        if i == 4:
+                            # date massage
+                            day_list = __val.split(" ")
+                            if len(day_list) == 3:
+                                if len(day_list[1].strip(",")) == 1:
+                                    day_list[1] = '0' + day_list[1].strip(",")
+                                else:
+                                    day_list[1] = day_list[1].strip(",")
+                                __val = "-".join(day_list)
+                            else:
+                                __val = "-".join(['Jan', '01', '1900'])
                         if not (i == 2 or i == 3):
                             ele_list.append(__val)
                     ipo_details[__key] = ele_list
-
+                    print("IPO details = ", ipo_details)
                 elif row%2 == 0:
                     divs = rows[row].find_all('div')[0]
                     sub_div = divs.descendants
                     i = 0
                     for div in sub_div:
                         if i == 5:
-                            day_data, url = get_listed_details(div)
+                            day_data = get_listed_details(div)
                             h.upd_dic_with_sub_list_ext(__key, day_data, ipo_details)
                         i += 1
-                print("IOP details = ", ipo_details)
+                # print("IOP details = ", ipo_details)
                 row += 1
     except Exception as err:
         traceback.print_exc()
@@ -177,3 +222,4 @@ def get_list_of_urls(url):
 if __name__ == "__main__":
     # get_listed_ipo(URL)
     get_ipo_details_perf('http://www.chittorgarh.com/ipo/ipo_perf_tracker.asp?FormIPOPT_Page=3')
+    # get_nse_code('http://www.chittorgarh.com/ipo/cochin-shipyard-ipo/684/')
